7e978285d415ed8a1b957b4873dd2d7a33d6ad81
Alutulu <arthur.mata24@gmail.com>
2024-01-29 16:51:46 +0100

    first commit

---
+++ Cyclindre.py
@@ -0,0 +1,49 @@
+import math
+
+class Cylindre:
+  last_id = 0
+  def __init__(self, x, y, type, rayon = 1):
+    self.id = Cylindre.last_id + 1
+    Cylindre.last_id += 1
+    self.x = x
+    self.y = y
+    self.rayon = rayon
+    self.type = None
+    self.connections = []
+    self.type = int(type)
+    if self.type == 1:
+      self.gain = 1
+      self.masse = 1
+    elif self.type == 2:
+      self.gain = 2
+      self.masse = 2
+    else:
+      self.gain = 3
+      self.masse = 2
+
+  def willCollide(self, cylindreDest, cylindreTest):
+    # équation droite de départ vers destination
+    a_eq = (cylindreDest.y - self.y) / (cylindreDest.x - self.x)
+    b_eq = self.y - self.x * a_eq
+    # équation de la perpendiculaire passant par le cylindre test
+    a_perp = -1/a_eq
+    b_perp = cylindreTest.y - a_perp * cylindreTest.x
+    # coordonnees du point d'intersection des 2 droites
+    x_inter = (b_perp - b_eq) / (a_eq - a_perp)
+    y_inter = x_inter * a_eq + b_eq
+    # vérifie si le point est dans les bornes min et max de départ et d'arrivée
+    x_min = min(self.x, cylindreDest.x)
+    x_max = max(self.x, cylindreDest.x)
+    y_min = min(self.y, cylindreDest.y)
+    y_max = max(self.y, cylindreDest.y)
+    if not(x_min <= x_inter and x_inter <= x_max and y_min <= y_inter and y_inter <= y_max):
+      return False
+    # distance entre les points
+    distance = math.sqrt((x_inter - cylindreTest.x)**2 + (y_inter - cylindreTest.y)**2)
+    return distance <= cylindreTest.rayon + self.rayon
+  
+  def updateConnections(self, listCylindres):
+    for i in range(len(listCylindres)):
+      if listCylindres[i] != self.id:
+        if not self.willCollide(listCylindres[i]):
+          self.connections.append(i)
---
+++ main.py
@@ -0,0 +1,16 @@
+from Cyclindre import Cylindre
+from LireMap import lireMap
+
+def main():
+    cylindres = []
+    x, y, t = lireMap("donnees-map.txt", afficherMap=False)
+    for i in range(len(x)):
+        cylindres.append(Cylindre(x[i], y[i], t[i]))
+    for cylindre in cylindres:
+        print(cylindre.id, cylindre.x, cylindre.y, cylindre.masse, cylindre.gain)
+    for i in range(len(cylindres)):
+        cylindres[i].updateConnections(cylindres)
+    print(cylindres[0].connections)
+
+if __name__ == "__main__":
+    main()
