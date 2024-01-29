903ecccf2ba987f0f3b5a9afe8ac883b8bbad78d
= <=>
2024-01-29 17:23:23 +0100

    calcul score

--- main.py
+++ main.py
@@ -1,5 +1,55 @@
 from Cyclindre import Cylindre
 from LireMap import lireMap
+a = 6.98*10**(-2)
+v0 = 1 
+b0 = 100
+b = 3
+alpha = 10**(-1) 
+qmax = 10000
+tmax = 100
+types = [
+        (1, 1),
+        (2, 2),
+        (3, 2)
+        ]
+
+def distance(p1, p2):
+    #Calcule la distance entre deux points
+    return np.sqrt((p2.x-p1.x)**2 + (p2.y-p1.y)**2)
+
+def vitesse(masse):
+    #Calcule la vitesse du robot
+    return v0*np.exp(-a*masse)
+
+def calculeScore(posini, chemin):
+    #Calcule le score de chaque chemin
+    pos = posini 
+    argent = 0
+    masse = 0
+    v = v0
+    carburant = qmax 
+    temps = 0
+    n_sommets = 0
+
+    if(list(set(chemin)) != chemin):
+        print("Attention ! Le chemin comporte des doublons")
+        exit()
+
+    for sommet in chemin:
+        dist = distance(pos, sommet)
+        carburant -= (b*masse + b0)*dist
+        temps += dist/vitesse(masse)
+        if(carburant < 0 or temps > tmax):
+            print(f"Carburant restant : {carburant}, temps restant : {tmax-temps} secondes")
+            print(f"Argent récolté : {argent}, Nombre de cylindres explorés : {n_sommets}")
+            return argent 
+        else:
+            n_sommets += 1
+            argent += sommet.gain
+            masse += sommet.masse
+            pos = sommet
+    print(f"Tous les sommets ont été parcourus, carburant restant : {carburant}, temps restant : {tmax-temps} secondes")
+    return argent
 
 def main():
     cylindres = []
@@ -14,3 +64,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
