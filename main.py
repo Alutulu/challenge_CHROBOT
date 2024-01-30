4f126912082870efe9e0919513750be7d0e566a4
Alutulu <arthur.mata24@gmail.com>
2024-01-30 15:51:20 +0100

    quick ajout parametre

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def main():
     dirIni = np.array([0, 1])
     res = planifie(chemin, posIni, dirIni)
     print(res)
-    afficherMap(cylindres, n_cylindres, styleVirgile=False)
+    afficherMap(cylindres, chemin=n_cylindres, afficherTousLesIndices=False, styleVirgile=False)
 
 if __name__ == "__main__":
     main()
